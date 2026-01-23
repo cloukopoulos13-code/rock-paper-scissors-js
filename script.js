007302fbb7e099e86d998f03e895a469ded40641
Christo Loukopoulos <249780393+cloukopoulos13-code@users.noreply.github.com>
2026-01-23 17:19:31 +0000

    Create player choice function
    Let player chooose ROCK, PAPER, SCISSORS and check for valid entry

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+
+//Gets the players choice for the game, converts to UPPER and checks if it's valid
+function getPlayerChoice()
+{
+    while (true){
+        choice = prompt("Rock, Paper, Scissors?").toUpperCase();    
+        
+        if (choice === "ROCK" || choice === "PAPER" || choice === "SCISSORS")
+        {
+            return choice;
+        }
+    }
+}
