7725dc62b564c8865793abca440f220fa33033fa
Christo Loukopoulos <249780393+cloukopoulos13-code@users.noreply.github.com>
2026-01-23 17:25:31 +0000

    Create computer logic
    Make function for computer to choose ROCK, PAPER or SCISSORS at random

--- script.js
+++ script.js
@@ -2,12 +2,32 @@
 //Gets the players choice for the game, converts to UPPER and checks if it's valid
 function getPlayerChoice()
 {
-    while (true){
+    while (true)
+    {
         choice = prompt("Rock, Paper, Scissors?").toUpperCase();    
         
         if (choice === "ROCK" || choice === "PAPER" || choice === "SCISSORS")
         {
             return choice;
         }
+    }
+}
+
+//Chooses a random number between 0 and 2, and assigns a choice based on that
+function getCompChoice()
+{
+    choice = Math.floor(Math.random() * 3);
+
+    if (choice === 0)
+    {
+        return "ROCK";
+    }
+    else if (choice == 1)
+    {
+        return "PAPER";
+    }
+    else
+    {
+        return "SCISSORS";
     }
 }
